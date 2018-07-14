c66dd14012b85a67a3cea8ee23962c53e648d7c9
Oleg Lytvyn <igocooper@gmail.com>
2018-07-14 19:46:10 +0300

    querying DB / creating and editing stores / creting lat and lng

--- learn-nodejs/controllers/storeController.js
+++ learn-nodejs/controllers/storeController.js
@@ -19,3 +19,33 @@ exports.createStore = async (req, res) => {
     req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`)
     res.redirect(`/store/${store.slug}`);
 }
+
+exports.updateStore = async (req, res) => {
+    // find and update the store
+    const store =  await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
+        new: true, //will return new store instead of old one
+        runValidators: true // will validate schema required filled
+    }).exec();
+
+    req.flash('success', `Successfully Updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}"> View Store</a>`);
+    res.redirect(`/stores/${store._id}/edit`);
+}
+
+exports.getStores = async (req, res) => {
+    // fetch stores data from DB
+    const stores = await Store.find();
+
+    res.render('stores', {
+        title: 'Stores',
+        stores
+    });
+}
+
+exports.editStore = async (req, res) => {
+    const store = await Store.findOne({_id: req.params.id});
+    // TODO:need to make sure it's an owner of the store
+    res.render('editStore',{
+        title: 'Edit Store',
+        store
+    });
+}
