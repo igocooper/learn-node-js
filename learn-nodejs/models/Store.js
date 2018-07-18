2ae512089b02fff700e419227b1696fc75fb70ed
Oleg Lytvyn <igocooper@gmail.com>
2018-07-18 21:46:37 +0300

    fix issue with similar slugs

--- learn-nodejs/models/Store.js
+++ learn-nodejs/models/Store.js
@@ -38,15 +38,25 @@ const storeSchema = new mongoose.Schema({
 
 // auto generate slug before save
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
     if(!this.isModified('name')) {
         next(); //skip it
         return;
     }
     // remember it should be not arrow function, cuz we need proper this refference to storeSchema object
     this.slug = slug(this.name);
+    // find other stores with same slugs if any
+    const slugRegExp = new RegExp(`^${this.slug}((-[0-9]*$)?)$`, 'i');
+
+    const storesWithSlug = await this.constructor.find({
+        slug: slugRegExp
+    });
+
+    // reasign slug if there are any matches 
+    if(storesWithSlug.length) {
+        this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+    }
     next();
-    // TODO: make more resiliant so slugs are unique 
 });
 
 module.exports = mongoose.model('Store', storeSchema);
